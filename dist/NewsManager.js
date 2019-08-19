863dcae2dacd82558172fd49eea60b3a88563c3d
yonikt <51288106+yonikt@users.noreply.github.com>
2019-08-19 10:43:24 +0300

    Update NewsManager.js

--- dist/NewsManager.js
+++ dist/NewsManager.js
@@ -3,7 +3,6 @@ class NewsManager {
         this.newsData = []
     }
 
-
     async getNewsData() {
         let x = await $.get(`/news`)
         this.newsData.push(x)
@@ -11,15 +10,5 @@ class NewsManager {
         return this.newsData[0]
     }
 
-
-    // async saveCity(cityName) {
-    //     let self = this.cityData
-    //     let data = await this.cityData.find(i => i.name === cityName)
-    //     await $.post('/city', data)
-    //     return data
-    // }
-
-
-   
 }
 
